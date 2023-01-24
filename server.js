3cd445a75f3e058caa7acd1cb4b95eb051183879
jordanmccollam <jordy.mccollam@gmail.com>
2023-01-24 15:59:08 -0600

    Fixed connection to client from server

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 const express = require("express");
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
 const app = express();
 
 const PORT = 8000;
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+app.use(bodyParser.json());
+
 app.get("/api", (req, res) => {
     res.json({"users": ["userOne", "userTwo", "userThree"]})
 })
+
+app.use(express.static('client/build'));
 
 app.listen(PORT, () => {
     console.log("Server started on port " + PORT)
