2912651cdd747c7a757f86b38b35689e7ce6e246
jordanmccollam <jordy.mccollam@gmail.com>
2023-01-24 16:15:36 -0600

    Made port an enviornment variable

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const bodyParser = require('body-parser');
 const cors = require('cors');
+require('dotenv').config();
 
 const app = express();
 
-const PORT = 8000;
+const PORT = process.env.PORT;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
