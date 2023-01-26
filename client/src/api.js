ad7774d72ad03430b6c73b9755bad1f46fca5cda
jordanmccollam <jordy.mccollam@gmail.com>
2023-01-25 18:05:13 -0600

    Minor api fixes

--- client/src/api.js
+++ client/src/api.js
@@ -3,14 +3,14 @@ import axios from 'axios';
 console.log("Api enviornment: " + process.env.REACT_APP_ENV); // <- for testing
 
 const rest = process.env.REACT_APP_ENV === 'production' ? axios : axios.create({
-    baseURL: 'http://localhost:8000/api'
+    baseURL: 'http://localhost:8000'
 })
 
 // TASKS
-export const getTasks = () => rest.get(`/tasks`).catch(err => console.error("request", err));
-export const createTask = (payload) => rest.post(`/task`, payload).catch(err => console.error("request", err));
-export const deleteTask = (id) => rest.delete(`/task/${id}`).catch(err => console.error("request", err));
-export const updateTask = (id, payload) => rest.put(`/task/${id}`, payload).catch(err => console.error("request", err));
+export const getTasks = () => rest.get(`/api/tasks`).catch(err => console.error("request", err));
+export const createTask = (payload) => rest.post(`/api/task`, payload).catch(err => console.error("request", err));
+export const deleteTask = (id) => rest.delete(`/api/task/${id}`).catch(err => console.error("request", err));
+export const updateTask = (id, payload) => rest.put(`/api/task/${id}`, payload).catch(err => console.error("request", err));
 
 
 const apis = {
